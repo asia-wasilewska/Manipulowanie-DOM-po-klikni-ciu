b17cdc0301e8a07a643283bcb443b13fe4c57fcc
Asia Wasilewska <asia_wasilewska@tlen.pl>
2017-05-04 21:53:05 +0200

    2 fixed list

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ var list = document.getElementById('list');
 var add = document.getElementById('addElem');
 
 add.addEventListener('click', function() {
-  var listId = document.getElementById('list'),
-      liElem = document.getElementsByTagName('li').length;
-  list.innerHTML += '<li>item '+ liElem + '</li>'
- });
+
+var liElem = list.getElementsByTagName('li').length;
+list.innerHTML += '<li>item '+ liElem + '</li>'
+});
